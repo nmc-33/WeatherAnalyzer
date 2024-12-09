38dadc2132bd6d3c7830bd7c9a39cdd2c1481b19
nmc-33 <car.nicholas02@gmail.com>
2024-12-09 18:32:44 -0500

    fix rabbitmq connection2

--- Backend/app/tasks.py
+++ Backend/app/tasks.py
@@ -15,11 +15,10 @@ def get_rabbitmq_connection():
 
     # Set up connection parameters for Pika
     connection_params = pika.ConnectionParameters(
-        host=url_parts.hostname,            # Extract hostname from the URL
-        port=5671,                # Extract port from the URL
-        virtual_host=url_parts.path[1:],    # Extract virtual host from the URL (remove the leading '/')
-        credentials=pika.PlainCredentials(url_parts.username, url_parts.password),
-        ssl = True  # Extract credentials
+        host=url_parts.hostname, 
+        port=5671,                
+        virtual_host=url_parts.path[1:],    
+        credentials=pika.PlainCredentials(url_parts.username, url_parts.password)
     )
 
     # Return the connection parameters
